ed3192b1360be8c4ebdbd923eeeeed47a0ec0abc
Styledev <12783+styledev@users.noreply.github.com>
2024-01-21 19:25:40 -0700

    Added standings HD ad

--- templates/page-tiers-nowrap.php
+++ templates/page-tiers-nowrap.php
@@ -17,8 +17,45 @@
   if ( $tiers_last ) $tiers_last = array_values($tiers_last);
   
   $sizes = [
-    'discord' => [ 'height' => 619, 'width' => 1100, 'heading_top' => 8, 'heading_size' => 48, 'gap' => 49, 'grid' => '75px 57px 54px 35px', 'sides' => 58, 'bottom' => 111, 'top' => 100, 'size' => 20 ],
-    'obs' => [ 'height' => 1080, 'width' => 1920, 'heading_top' => 13, 'heading_size' => 72, 'gap' => 81, 'grid' => '150px 114px 108px 70px', 'sides' => 100, 'bottom' => 192, 'top' => 173, 'size' => 42 ],
+    'hd'      => [
+      'stats'        => FALSE,
+      'height'       => 1020,
+      'width'        => 684,
+      'heading_top'  => 290,
+      'heading_size' => 48,
+      'gap'          => 0,
+      'grid'         => '1fr',
+      'sides'        => 41,
+      'bottom'       => 80,
+      'top'          => 430,
+      'size'         => 28,
+    ],
+    'discord' => [
+      'stats'        => TRUE,
+      'height'       => 619,
+      'width'        => 1100,
+      'heading_top'  => 8,
+      'heading_size' => 48,
+      'gap'          => 49,
+      'grid'         => '75px 57px 54px 35px',
+      'sides'        => 58,
+      'bottom'       => 111,
+      'top'          => 100,
+      'size'         => 20
+    ],
+    'obs'     => [
+      'stats'        => TRUE,
+      'height'       => 1080,
+      'width'        => 1920,
+      'heading_top'  => 13,
+      'heading_size' => 72,
+      'gap'          => 81,
+      'grid'         => '150px 114px 108px 70px',
+      'sides'        => 100,
+      'bottom'       => 192,
+      'top'          => 173,
+      'size'         => 42
+    ],
   ];
 ?>
 <script src="<?php echo RES ?>/js/html2canvas.min.js"></script>
@@ -37,28 +74,24 @@
   .tier{display: grid;}
   .tier__teams{list-style-type:none;margin:0;padding:0;}
   .tier__team{align-items:center;display:grid;justify-content:space-between;}
-  .tier__team + .tier__team{border-top:2px solid #F81EFF;}
+  
+  .box--discord .tier__team + .tier__team,
+  .box--obs .tier__team + .tier__team{border-top:2px solid #F81EFF;}
   .tiers + canvas{padding:0;}
   .tiers .wp-block-group__inner-container{display:flex;}
   .tier span, .tier strong{flex:0 0 33%;}
   .tier span{text-align:center;}
+  
   .tier strong{text-align:right;padding-right:6px;}
+  
+  .box--hd .tier strong{text-align:center;}
+  .box--hd .tier strong span{display:inline-block;text-align:center;width:30px;}
+  .box--hd .tier strong span:empty{display:none;}
 </style>
 
 <div>
   <?php
     foreach ($sizes as $type => $args) {
-      echo "
-        <style>
-          .box--$type{ min-height:{$args['height']}px;min-width:{$args['width']}px; height:{$args['height']}px;width:{$args['height']}px; }
-          .box--$type .clone{height:{$args['height']}px;width:{$args['width']}px;}
-          .box--$type .clone > img{height:{$args['height']}px!important;width:{$args['width']}px!important;}
-          .box--$type .clone .title{font-size:{$args['heading_size']}px;top:{$args['heading_top']}px;}
-          .box--$type .tiers{bottom:{$args['bottom']}px;font-size:{$args['size']}px;grid-column-gap:{$args['gap']}px;left:{$args['sides']}px;right:{$args['sides']}px;top:{$args['top']}px;}
-          .box--$type .tier__team{grid-template-columns:{$args['grid']};}
-        </style>
-      ";
-      
       $tiers_rows = '';
       
       if ( $tiers ) {
@@ -94,22 +127,41 @@
             }
             else $change = '';
             
-            $teams[] = sprintf('<strong>%s</strong><span>%s</span><span>%s</span><span>%s</span>', $team['name'], $team['mmr'], $team['sr'], $change);
+            if ( $args['stats'] ) {
+              $teams[] = sprintf('<strong>%s</strong><span>%s</span><span>%s</span><span>%s</span>', $team['name'], $team['mmr'], $team['sr'], $change);
+            }
+            else {
+              $teams[] = sprintf('<strong>%s <span>%s</span></strong>', $team['name'], $change);
+            }
           }
           
           $tiers_rows .= sprintf(
             '<div class="tier tier--%s">
-              <div class="tier__team tier__team--header"><strong></strong><span>MMR</span><span>SR</span><span></span></div>
+              %s
               <div class="tier__team">%s</div>
             </div>',
-            ucwords($tier), implode('</div><div class="tier__team">', $teams)
+            ucwords($tier),
+            $args['stats'] ? '<div class="tier__team tier__team--header"><strong></strong><span>MMR</span><span>SR</span><span></span></div>' : '',
+            implode('</div><div class="tier__team">', $teams)
           );
         }
       }
       
+      $styles = "
+        <style>
+          .box--$type{ min-height:{$args['height']}px;min-width:{$args['width']}px;height:{$args['height']}px;width:{$args['width']}px; }
+          .box--$type .clone{height:{$args['height']}px;width:{$args['width']}px;}
+          .box--$type .clone > img{height:{$args['height']}px!important;width:{$args['width']}px!important;}
+          .box--$type .clone .title{font-size:{$args['heading_size']}px;top:{$args['heading_top']}px;}
+          .box--$type .tiers{bottom:{$args['bottom']}px;font-size:{$args['size']}px;grid-column-gap:{$args['gap']}px;left:{$args['sides']}px;right:{$args['sides']}px;top:{$args['top']}px;}
+          .box--$type .tier__team{grid-template-columns:{$args['grid']};}
+        </style>
+      ";
+      
       printf(
         '
           <div class="box box--%s">
+            %s
             <div class="clone">
               <div class="title">STANDINGS S%s C%s</div>
               <div class="tiers">
@@ -120,12 +172,15 @@
           </div>
         ',
         $type,
+        $styles,
         $season, $cycle,
         $tiers_rows,
         RES . '/images/dl-tiers-' . $type . '-' . ($_GET['style'] ?? 'a')
       );
     }
   ?>
+  
+  
 </div>
 
 <script>
